e6c549295beddc1f2633ada7fc885667d7c1c86d
angieccr12 <accallejasr@udistrital.edu.co>
2025-08-08 15:20:47 -0500

    [UPDATE]: cors to front url

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://focus-track-frontend-two.vercel.app'
+  ],
   credentials: true
 }));
-
 // Rutas
 const pool = require('./db');
 const activityRoutes = require('./routes/activityRoutes.js');
